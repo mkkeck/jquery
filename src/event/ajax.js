6350b42993311a1b2be741f6b7bac79a0176f838
mk.keck <me@michaelkeck.de>
2017-10-30 11:49:39 +0100

    Event: [].forEach is faster than $.each

--- src/event/ajax.js
+++ src/event/ajax.js
@@ -4,14 +4,14 @@ define( [
 ], function( jQuery ) {
 
 // Attach a bunch of functions for handling common AJAX events
-jQuery.each( [
+[
 	"ajaxStart",
 	"ajaxStop",
 	"ajaxComplete",
 	"ajaxError",
 	"ajaxSuccess",
 	"ajaxSend"
-], function( i, type ) {
+].forEach( function( type ) {
 	jQuery.fn[ type ] = function( fn ) {
 		return this.on( type, fn );
 	};
--- src/event/alias.js
+++ src/event/alias.js
@@ -5,10 +5,10 @@ define( [
 	"./trigger"
 ], function( jQuery ) {
 
-jQuery.each( ( "blur focus focusin focusout load resize scroll unload click dblclick " +
+( "blur focus focusin focusout load resize scroll unload click dblclick " +
 	"mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
-	"change select submit keydown keypress keyup error contextmenu" ).split( " " ),
-	function( i, name ) {
+	"change select submit keydown keypress keyup error contextmenu"
+).split( " " ).forEach( function( name ) {
 
 	// Handle event binding
 	jQuery.fn[ name ] = function( data, fn ) {
