434b3e7aec659ae6f08a144eaa9481d35c1327a6
mk.keck <me@michaelkeck.de>
2017-10-30 11:40:11 +0100

    Dimensions: Don't repeat your self

--- src/dimensions.js
+++ src/dimensions.js
@@ -15,14 +15,17 @@ jQuery.each( { Height: "height", Width: "width" }, function( name, type ) {
 				extra = defaultExtra || ( margin === true || value === true ? "margin" : "border" );
 
 			return access( this, function( elem, type, value ) {
-				var doc;
+				var doc,
+					clientname = "client" + name,
+					offsetname = "offset" + name,
+					scrollname = "scroll" + name;
 
 				if ( jQuery.isWindow( elem ) ) {
 
 					// As of 5/8/2012 this will yield incorrect results for Mobile Safari, but there
 					// isn't a whole lot we can do. See pull request at this URL for discussion:
 					// https://github.com/jquery/jquery/pull/764
-					return elem.document.documentElement[ "client" + name ];
+					return elem.document.documentElement[ clientname ];
 				}
 
 				// Get document width or height
@@ -32,9 +35,9 @@ jQuery.each( { Height: "height", Width: "width" }, function( name, type ) {
 					// Either scroll[Width/Height] or offset[Width/Height] or client[Width/Height],
 					// whichever is greatest
 					return Math.max(
-						elem.body[ "scroll" + name ], doc[ "scroll" + name ],
-						elem.body[ "offset" + name ], doc[ "offset" + name ],
-						doc[ "client" + name ]
+						elem.body[ scrollname ], doc[ scrollname ],
+						elem.body[ offsetname ], doc[ offsetname ],
+						doc[ clientname ]
 					);
 				}
 
